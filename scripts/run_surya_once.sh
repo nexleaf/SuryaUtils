1763c48095232ed4d41400c46a691518846e6d0e
Martin Lukac <martin@nexleaf.org>
2012-04-10 14:37:29 -0700

    added ability to run once

---
+++ scripts/run_surya_once.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+####
+#### TODO:
+## Track pid's of python processes ($! will be 'the process ID of the most recently executed background (asynchronous) command'
+## Write signal handler to catch the Ctrl-C signal and kill those procs
+#
+
+
+
+
+source /home/surya/deployed/set_surya_path.sh
+
+cd $SURYA_DEPLOY_ROOT
+
+
+# load any new calibrations
+/usr/bin/python2.6 -u $SURYA_DEPLOY_ROOT/SuryaWebPortal/src/SuryaWebPortal/manage.py start $SURYA_DEPLOY_ROOT/SuryaWebPortal/src/SuryaWebPortal/calibrationfile
+
+# Just touch files, hopefully mod_wsgi's daemon mode will reload everything
+touch $SURYA_DEPLOY_ROOT/SuryaWebPortal/src/SuryaWebPortal/django.wsgi
+touch $SURYA_DEPLOY_ROOT/SuryaWebPortal/src/SuryaWebPortal/settings.py
+
+
+/usr/bin/python2.6 -u $SURYA_DEPLOY_ROOT/SuryaIANAGmailPortal/src/GmailMonitor/IANAGmailMonitor.py -1 &> /dev/null &
+GMM=$!
+
+sleep 5
+
+/usr/bin/python2.6 -u $SURYA_DEPLOY_ROOT/SuryaIANAFramework/src/IANA/IANAFramework.py -1 &> /dev/null &
+SIF=$!
+
+sleep 15
+
+/usr/bin/python2.6 -u $SURYA_DEPLOY_ROOT/SuryaIANAGmailPortal/src/GmailResults/IANAGmailResults.py -1 &> /dev/null &
+GMR=$!
+
+sleep 5
+
+
+# Let's see if closure type things work here
+# function handle_sig() {
+#     echo Killing PIDs $GMM $GMR $SIF
+#     kill $GMM
+#     kill $GMR
+#     kill $SIF
+#     echo "Killed..."
+#     echo ""
+#     exit 0
+# }
+
+# echo Setting trap for PIDs $GMM $GMR $SIF
+
+# trap handle_sig SIGHUP SIGINT SIGTERM
+
+#cd $SURYA_DEPLOY_ROOT/SuryaWebPortal/src/SuryaWebPortal/
+
