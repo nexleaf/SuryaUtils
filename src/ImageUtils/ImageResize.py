c577e483cd60eb6ed9bbc7cd98ca5d3843142e23
Martin Lukac <martin@nexleaf.org>
2012-05-16 20:54:33 +0000

    added image rotate and fixed instance check

--- src/ImageUtils/ImageResize.py
+++ src/ImageUtils/ImageResize.py
@@ -6,6 +6,8 @@ import errno
 import os.path
 import logging
 import StringIO
+import PIL
+import PIL.JpegImagePlugin
 import PIL.Image as Image
 import PIL.ImageDraw as ImageDraw
 import PIL.ImageFont as ImageFont
@@ -17,14 +19,46 @@ log = getLog("ImageResize")
 log.setLevel(logging.ERROR)
 
 
-def imageResize(imagefile, largestside):
-    '''Resize an image to what is specified as the long side'''
+def imageRotate(imagefile, deg):
+    '''Rotate image'''
     # check for string or a mongo gridfs file object
+    log.error("imagefile type is: " + str(imagefile.__class__))
     if isinstance(imagefile, str):
         if not os.path.isfile(imagefile):
             log.error('imagefile ' + imagefile + ' does not exist', extra=tags)
             return None, ExitCode.FileNotExists
         image = Image.open(imagefile)
+    elif isinstance(imagefile, PIL.JpegImagePlugin.JpegImageFile) or isinstance(imagefile, PIL.Image.Image):
+        image = imagefile
+    else:
+        # assume is already PIL type
+        image = Image.open(StringIO.StringIO(imagefile.read()))
+        #image = imagefile
+    
+    (x, y) = image.size
+    log.error("In size: %d %d" % (x, y))
+
+    newimage = image.rotate(deg, Image.BILINEAR, 1)
+
+    (x, y) = newimage.size
+
+    log.error("Out size: %d %d" % (x, y))
+
+    return newimage
+    
+
+
+def imageResize(imagefile, largestside):
+    '''Resize an image to what is specified as the long side'''
+    # check for string or a mongo gridfs file object
+    log.error("imagefile type is: " + str(imagefile.__class__))
+    if isinstance(imagefile, str):
+        if not os.path.isfile(imagefile):
+            log.error('imagefile ' + imagefile + ' does not exist', extra=tags)
+            return None, ExitCode.FileNotExists
+        image = Image.open(imagefile)
+    elif isinstance(imagefile, PIL.JpegImagePlugin.JpegImageFile) or isinstance(imagefile, PIL.Image.Image):
+        image = imagefile
     else:
         # assume is already PIL type
         image = Image.open(StringIO.StringIO(imagefile.read()))
